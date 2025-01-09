e3b3eff866d31c01d8103e3c39e14380a1a2108a
AlexArce2000 <alexisroarce2000@gmail.com>
2025-01-09 20:13:59 -0300

    dato ingreso egreso

---
+++ js/Egreso.js
@@ -0,0 +1,7 @@
+class Egreso extends Dato{
+    static contadorEgresos = 0;
+    constructor(descripcion, valor){
+        super(descripcion,valor);
+        this._id = ++Egreso.contadorEgresos;
+    }
+}
