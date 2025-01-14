803a90f0db6abd879677f85694f11950a5f642bf
AlexArce2000 <alexisroarce2000@gmail.com>
2025-01-14 02:24:35 -0300

    listado de ingresos

--- js/app.js
+++ js/app.js
@@ -12,6 +12,35 @@ let cargarApp = ()=>{
     cargarCabecero();
 }
 
+let totalIngresos = ()=>{
+    let totalIngresos=0;
+    for (const ingreso of ingresos) {
+        totalIngresos += ingreso.valor;
+    }
+    return totalIngresos;
+}
+
+let totalEgresos= ()=>{
+    let totalEgresos = 0;
+    for (const egreso of egresos) {
+        totalEgresos += egreso.valor;
+    }
+    return totalEgresos;
+}
+
 let cargarCabecero= ()=>{
-    
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor)=>{
+    return valor.toLocaleString('es-PY',{style:'currency', currency:'PYG'})
+}
+
+const formatoPorcentaje = (valor) =>{
+    return valor.toLocaleString('es-PY',{style:'percent', minimunFractionDigits:2})
 }
